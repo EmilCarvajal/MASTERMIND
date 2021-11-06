3bcdb9d428935d76ea4feac7a249675d5a00758c
emilsj2 <57761053+emilsj2@users.noreply.github.com>
2021-11-06 19:56:14 +0100

    board up 1

--- src/main/java/mastermind/Board.java
+++ src/main/java/mastermind/Board.java
@@ -1,2 +1,105 @@
-public class Board {
+package mastermind;
+
+import java.util.ArrayList;
+
+import static mastermind.MasterMindGame.MAX_OPPORTUNITIES;
+
+//NO NECESSARIA DE TESTEJAR, NOMES FA PRINTS
+//clase encargada de printar el tauler mostrant cada intent amb la seva pista corresponent
+public class Board{
+
+    //	historial de codis introduits per el jugador
+    private ArrayList<Code> m_listCodigos;
+    //	historial de pistes introduits per el jugador
+    private ArrayList<Hint> m_listEnciertos;
+    private String tauler;
+
+    public Board() {
+//		inicialitzacio de les variable
+        m_listCodigos = new ArrayList<Code>();
+        m_listEnciertos = new ArrayList<Hint>();
+    }
+
+    //	GETTER
+    public ArrayList<Code> getHistorialCodis() {
+//		retorna el historial de codis com una ArrayList
+        return this.m_listCodigos;
+    }
+
+    public ArrayList<Hint> getHistorialPistes() {
+//		retorna el historial de pistes com una ArrayList
+        return this.m_listEnciertos;
+    }
+
+    //	SETTER
+    public void afegirCodiHistorial(Code codigo) {
+        this.m_listCodigos.add(codigo);
+    }
+
+    //	SETTER
+    public void afegirPistaHistorial(Hint pista) {
+        this.m_listEnciertos.add(pista);
+
+    }
+
+    public void mostrarTablero() {
+        String tablero = "";
+        tablero+="-----------------------------------------\n";
+        tablero+="            MASTERMIND\n";
+        tablero+="-----------------------------------------\n";
+        tablero += "+-------------------+\n";
+        tablero += "| intents | pistes  |\n";
+        tablero += "+-------------------+\n";
+        for (int i = 0; i < MAX_OPPORTUNITIES; i++) {
+            if (this.m_listCodigos.size() > i) {
+                tablero += "| ";
+                for (String codiChar : m_listCodigos.get(i).getCodi().split("")) {
+                    tablero += codiChar + " ";
+                }
+                tablero += ": ";
+                for (String pistaChar : m_listCodigos.get(i).getPista().split("")) {
+                    tablero += pistaChar + " ";
+                }
+                tablero += "|\n";
+            } else {
+                tablero += "| - - - - : - - - - |\n";
+            }
+        }
+        tauler += "+-------------------+";
+        setTauler(tauler);
+    }
+
+    public void setTauler() {
+        String tauler = "";
+        tauler += "+-------------------+\n";
+        tauler += "| intents | pistes  |\n";
+        tauler += "+-------------------+\n";
+        for (int i = 0; i < Mastermind.MAX_INTENTS; i++) {
+            if (this.historialCodis.size() > i) {
+                tauler += "| ";
+                for (String codiChar : historialCodis.get(i).getCodi().split("")) {
+                    tauler += codiChar + " ";
+                }
+                tauler += ": ";
+                for (String pistaChar : historialPistes.get(i).getPista().split("")) {
+                    tauler += pistaChar + " ";
+                }
+                tauler += "|\n";
+            } else {
+                tauler += "| - - - - : - - - - |\n";
+            }
+        }
+        tauler += "+-------------------+";
+        setTauler(tauler);
+    }
+
+    public String getTauler() {
+        return tauler;
+    }
+
+    public void setTauler(String tauler) {
+        this.tauler = tauler;
+    }
+
+
 }
--- src/main/java/mastermind/Code.java
+++ src/main/java/mastermind/Code.java
@@ -1,4 +1,4 @@
 package mastermind;
 
-public class Hint {
+public class Code {
 }
